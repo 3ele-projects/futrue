eb7ffd97a38cb5f9e974b1afb8af58cc8381273e
3ele-projects <github@3ele.de>
2020-08-17 20:16:30 +0200

    add headline to hero image block

--- blocks/hero-image/block.php
+++ blocks/hero-image/block.php
@@ -38,17 +38,23 @@ if( !empty($block['align']) ) {
 }
 $background_color = get_field('background-color');
 $col_align = get_field('align');
+$headline = get_field('headline');
+$subheadline = get_field('subheadline');
+$description = get_field('description');
+$blockimage = get_field('block_image');
+$font_color = get_field('font-color');
+$anker_id = get_field('anker_id');
 
 ?>
-<section id="<?php echo esc_attr($id); ?>" class="hero-image <?php echo esc_attr($className); ?>">
+<section id="<?php echo esc_attr($anker); ?>" class="hero-image <?php echo esc_attr($className); ?>">
 <div id="particles-js"></div>
 
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
 
-        <h1>	<?php the_field( 'hero-text-headline' ); ?></h1>
-        <p class="h2"><?php the_field( 'hero-text-subheadline' ); ?></p>
+        <h1>	<?php echo $headline; ?></h1>
+        <p class="h2"><?php echo $subheadline; ?></p>
         <?php if ( have_rows( 'cta' ) ) : ?>
 		<?php while ( have_rows( 'cta' ) ) : the_row(); ?>
         <p><a href="<?php the_sub_field( 'link' ); ?>" class=" m-3 btn btn-primary btn-lg"><?php the_sub_field( 'linktext' ); ?> </a></p>
